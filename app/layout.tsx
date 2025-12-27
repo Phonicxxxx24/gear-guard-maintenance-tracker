bfca2289f568496dbbac8741e3f22e1dd4d625cb
Yashvi-2007 <yashvikansara789@gmail.com>
2025-12-27 15:19:23 +0530

    Remove RootLayout component
    Removed the RootLayout component from layout.tsx.

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,17 +30,3 @@ export const metadata: Metadata = {
   },
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return (
-    <html lang="en">
-      <body className={`font-sans antialiased`}>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  )
-}
